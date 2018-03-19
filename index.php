1c7312817bfbca0aa857b06efd07316c6a42ce8a
ForumEnsao <37407603+ForumEnsao@users.noreply.github.com>
2018-03-19 13:41:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a href="index.html"><img src="img/logo.png" alt="logo"></a>
+              <a href="http://forumensao.com"><img src="img/logo.png" alt="logo"></a>
             </div>
     
             <div class="collapse navbar-collapse" id="nav">
@@ -191,7 +191,7 @@
                             data-endelementdelay="0.1"
                             data-endspeed="500"
                             data-endeasing="Power4.easeIn"
-                            style="z-index: 2; max-width: auto; max-height: auto; white-space: nowrap;">SKY HIGH
+                            style="z-index: 2; max-width: auto; max-height: auto; white-space: nowrap;">Salon de l'innovation
                         </div>
                 
                         <!-- LAYER NR. 2 -->
@@ -207,51 +207,12 @@
                             data-endelementdelay="0.1"
                             data-endspeed="500"
                             data-endeasing="Power4.easeIn"
-                            style="z-index: 3; max-width: auto; max-height: auto; white-space: nowrap;">Imperfection is beauty, madness<br/> is genius and it's better to be<br/> absolutely ridiculous than<br/> absolutely boring.
+                            style="z-index: 3; max-width: auto; max-height: auto; white-space: nowrap;">La nouveauté de cette année<br/><br/><br/>.
                         </div>
                 
                         
                     </li>
-                    
-        			<!-- SLIDE 4 -->
-                    <li data-transition="slidevertical" data-slotamount="1" data-masterspeed="1000" data-thumb="img/slide_thumb_4.jpg"  data-saveperformance="off"  data-title="Slide">
-                        <!-- MAIN IMAGE -->
-                        <img src="img/slide4.jpg"  alt="fullslide7"  data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
-                        <!-- LAYERS -->
-                
-                        <!-- LAYER NR. 1 -->
-                        <div class="tp-caption light_heavy_70_shadowed lfb ltt tp-resizeme"
-                            data-x="center" data-hoffset="270"
-                            data-y="center" data-voffset="-70"
-                            data-speed="600"
-                            data-start="800"
-                            data-easing="Power4.easeOut"
-                            data-splitin="none"
-                            data-splitout="none"
-                            data-elementdelay="0.01"
-                            data-endelementdelay="0.1"
-                            data-endspeed="500"
-                            data-endeasing="Power4.easeIn"
-                            style="z-index: 2; max-width: auto; max-height: auto; white-space: nowrap;">NULLA COMMODO
-                        </div>
-                
-                        <!-- LAYER NR. 2 -->
-                        <div class="tp-caption light_medium_30_shadowed lfb ltt tp-resizeme"
-                            data-x="center" data-hoffset="220"
-                            data-y="center" data-voffset="0"
-                            data-speed="600"
-                            data-start="900"
-                            data-easing="Power4.easeOut"
-                            data-splitin="none"
-                            data-splitout="none"
-                            data-elementdelay="0.01"
-                            data-endelementdelay="0.1"
-                            data-endspeed="500"
-                            data-endeasing="Power4.easeIn"
-                            style="z-index: 3; max-width: auto; max-height: auto; white-space: nowrap;">As long as there was coffee in the<br/> world, how bad could things be?
-                        </div>
-                        
-                    </li>
+             
         
     			</ul>		
                 <div class="tp-bannertimer"></div>	
