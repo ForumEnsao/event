24b57078e9394631d4ed2e5ec7639abb4868c89d
ForumEnsao <37407603+ForumEnsao@users.noreply.github.com>
2018-03-20 11:20:28 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1524,19 +1524,7 @@
 
         
         <!-- google map -->
-		<div id="map"></div>
-		<script>
-function myMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    }
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
-</script>
-	<script src="https://maps.googleapis.com/maps/api/js?callback=myMap"></script>
-
+		<div id="gmap_canvas"></div>
         
         <!-- FOOTER -->        
         <footer id="footer">
