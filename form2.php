cf2d1ab4570304f218d6203a4272c29923bba795
AnasHafsi <Anashafsii@gmail.com>
2018-03-24 22:52:55 +0000

    Formulaire 2

--- form2.php
+++ form2.php
@@ -9,7 +9,7 @@ $Num = mysqli_real_escape_string($link, $_REQUEST['Num']);
 $Email = mysqli_real_escape_string($link, $_REQUEST['Email']);
 $pack = mysqli_real_escape_string($link, $_REQUEST['pack']);
  
-$sql = "INSERT INTO persons (Nom_P, Num, Email, pack) VALUES ($Nom_P, '$Num', '$Email', '$pack')";
+$sql = "INSERT INTO stand (Nom_P, Num, Email, pack) VALUES ($Nom_P, '$Num', '$Email', '$pack')";
 if(mysqli_query($link, $sql)){
 	$message = "Enregistrement effectué avec succès ";
 	echo "<script type='text/javascript'>alert('$message');</script>";
