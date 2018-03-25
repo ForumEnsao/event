ddbd866938c9f38a01c7993a9619fbc269bcfdee
Anas Hafsi <AnasHafsii@gmail.com>
2018-03-25 00:30:20 +0000

    Update form2.php

--- form2.php
+++ form2.php
@@ -13,8 +13,7 @@ $sql = "INSERT INTO stand (Nom_P, Num, Email, pack) VALUES ('$Nom_P', '$Num', '$
 if(mysqli_query($link, $sql)){
 	$message = "Enregistrement effectué avec succès ";
 	echo "<script type='text/javascript'>alert('$message');</script>";
-	header("location: index.php");
-    echo "Records inserted successfully.";
+	header("location: forumensao.com");
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
